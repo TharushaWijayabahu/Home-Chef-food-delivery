7e4280520f50f1bf42091fc4713ce2a3ffa88804
InuriIlangakoon <47524614+InuriIlangakoon@users.noreply.github.com>
2021-01-11 10:51:27 +0530

    Update login page

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -2,10 +2,10 @@ function Login() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     if (email == "user@gmail.com" && password == "123") {
-        window.location.href = "../main/homepage.html"
+        window.location.href = "../HomeUser"
     }
     else if (email == "vendor@gmail.com" && password == "456") {
-        window.location.href = "../vendor/notifications.html"
+        window.location.href = "../HomeVendor"
     }
     else {
         alert("Invalid username and password");
