5d1c60bfd9286cadaf68b03be3b6c11822b95d3a
Tharusha Wijayabahu <Tharusha>
2021-01-16 17:11:31 +0530

    Update Loyalty points logic

--- pages/UserAccount/userAccount.js
+++ pages/UserAccount/userAccount.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     let totalPoint = localStorage.getItem("totalPoint");
-    totalPoint == null || totalPoint == undefined ? totalPoint = 0 : totalPoint = totalPoint
+    totalPoint == null || totalPoint == undefined ? totalPoint = 0 : totalPoint = totalPoint + 200
     console.log('totalPoint : ', totalPoint)
     $('#totalPoint').text(totalPoint);
 })
