1321a0ac76fe11ba40895c1ad13dec2199f17f6d
InuriIlangakoon <47524614+InuriIlangakoon@users.noreply.github.com>
2021-01-16 17:23:39 +0530

    Create redeem points function

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -2,6 +2,8 @@ function Login() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     if (email == "user@gmail.com" && password == "123") {
+        localStorage.setItem("totalPoint", 30)
+        //console.log(localStorage.getItem("totalPoint"))
         window.location.href = "../HomeUser"
     }
     else if (email == "vendor@gmail.com" && password == "456") {
