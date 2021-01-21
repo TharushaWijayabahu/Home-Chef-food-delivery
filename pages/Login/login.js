b6190e31dfbf7afa026e83bcd993dc15a2277e2b
Tharusha Wijayabahu <Tharusha>
2021-01-21 22:21:42 +0530

    Upadte login URl

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -5,17 +5,9 @@ function Login() {
         localStorage.setItem("totalPoint", 30)
             //console.log(localStorage.getItem("totalPoint"))
         window.location.href = "../HomeUser"
-<<<<<<< HEAD
     } else if (email == "vendor@gmail.com" && password == "456") {
         window.location.href = "../VendorOrders/newOrder.html"
     } else {
-=======
-    }
-    else if (email == "vendor@gmail.com" && password == "456") {
-        window.location.href = "../VendorOrders/newOrder.html"
-    }
-    else {
->>>>>>> 97dbafe51b4df187ee5a87084b1e9dbe93b2f2f1
         alert("Invalid username and password");
     }
 }
